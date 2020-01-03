e4fd4cec44e05c41f9e0d18d3413efb2cb8a821a
Brendan Higgins <bhig93@gmail.com>
2020-01-03 18:58:54 -0500

    Generate certificates and send emails from CSV

--- generate_certificates.sh
+++ generate_certificates.sh
@@ -1,20 +1,28 @@
 #! /bin/sh
 
 template_name=$1
-new_name=$2
+csv_name=$2
 rendered_doc="tmp.docx"
 rendered_pdf="tmp.pdf"
 template_string="AttendeeName"
 
 mkdir tmp/
+mkdir generated_certificates/
 unzip "$template_name" -d tmp/
 cp tmp/word/document.xml document.xml
-sed -i "s/$template_string/$new_name/g" tmp/word/document.xml
-cd tmp/ && zip -r ../$rendered_doc * && cd ..
-cp document.xml tmp/word/document.xml
-libreoffice --headless --convert-to pdf $rendered_doc
+while IFS=, read -r last_name first_name email
+do
+  name="$first_name $last_name"
+  qualified_file_name="${first_name}_$last_name.pdf"
+  echo $name
+  sed -i "s/$template_string/$name/g" tmp/word/document.xml
+  cd tmp/ && zip -r ../$rendered_doc * && cd ..
+  cp document.xml tmp/word/document.xml
+  libreoffice --headless --convert-to pdf $rendered_doc
+  mv $rendered_pdf $qualified_file_name
+  echo "Here is your certificate." | mail -s "Test Certificate" -a $qualified_file_name $email
+  mv $qualified_file_name generated_certificates/
+  rm $rendered_doc
+done < $csv_name
 rm -rf tmp/
 rm document.xml
-rm $rendered_doc
-echo "Here is your certificate." | mail -s "Test Certificate" -a $rendered_pdf bhig93@gmail.com
-rm $rendered_pdf
