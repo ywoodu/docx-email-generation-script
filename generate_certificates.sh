b018b7b2965eb38d6cb03da328a23a987dc77e09
Shelly Higgins <shellyhiggins@shellys-mbp.lan>
2020-04-28 02:10:52 -0400

    Got generating and sending certificates working on mac

--- generate_certificates.sh
+++ generate_certificates.sh
@@ -1,10 +1,12 @@
-#! /bin/sh
+#! /bin/bash
 
 template_name=$1
 csv_name=$2
 rendered_doc="tmp.docx"
 rendered_pdf="tmp.pdf"
 template_string="AttendeeName"
+libreoffice="/Applications/LibreOffice.app/Contents/MacOS/soffice"
+bcc="3482954@bcc.hubspot.com"
 
 mkdir tmp/
 mkdir generated_certificates/
@@ -15,14 +17,15 @@ do
   name="$first_name $last_name"
   qualified_file_name="${last_name}_$first_name.pdf"
   echo $name
-  sed -i "s/$template_string/$name/g" tmp/word/document.xml
+  sed -i '' "s/$template_string/$name/g" tmp/word/document.xml
   cd tmp/ && zip -r ../$rendered_doc * && cd ..
   cp document.xml tmp/word/document.xml
-  libreoffice --headless --convert-to pdf $rendered_doc
-  mv $rendered_pdf $qualified_file_name
-  echo "Here is your certificate." | mail -s "Test Certificate" -a $qualified_file_name $email
-  mv $qualified_file_name generated_certificates/
+  $libreoffice --headless --convert-to pdf $rendered_doc
+  mv "$rendered_pdf" "$qualified_file_name"
+  # cat message.html | mail -a "Content-Type: text/html" -s "$(echo -e "Certificate\nBcc:$bcc")" -A $qualified_file_name $email
+  mutt -e 'set content_type="text/html"' -s 'Certificate for Solar Ready Continuing Education Webinar - S-5!' -b $bcc -a "$qualified_file_name" -- "$email" <message.html
+  mv "$qualified_file_name" generated_certificates/
   rm $rendered_doc
-done < $csv_name
+done < "$csv_name"
 rm -rf tmp/
 rm document.xml
